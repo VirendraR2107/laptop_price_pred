ec37cd692d475b4bdd898749adc9280d20f0d788
VirendraR2107 <160562981+VirendraR2107@users.noreply.github.com>
2024-04-25 09:48:02 +0530

    laptop.py

--- laptop.py
+++ laptop.py
@@ -7,7 +7,7 @@ lr=pickle.load(open('lr1_model_23_apr.pkl','rb'))
 dt=pickle.load(open('dt1_model_23_apr.pkl','rb'))
 rf=pickle.load(open('rf1_model_23_apr.pkl','rb'))
 
-df=pickle.load('df_23_apr.pkl')
+df=pickle.load(open('df_23_apr.pkl','rb'))
 
 st.title('Laptop Price Prediction')
 st.header('Fill the laptop details to predict the price')
@@ -15,7 +15,7 @@ st.header('Fill the laptop details to predict the price')
 Company=st.selectbox('Company',df['Company'].unique())
 TypeName=st.selectbox('TypeName',df['TypeName'].unique())
 Ram=st.selectbox('Ram',df['Ram'].unique())
-Weight=st.number_input('Weight(Between 0.5-4.3)')
+Weight=st.number_input('Weight(Between 0.5 - 4.3)')
 Touchscreen=st.selectbox('Touchscreen',df['Touchscreen'].unique())
 
 
